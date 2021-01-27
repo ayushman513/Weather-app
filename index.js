3c1ea83780b6eceb537f66055655fdf7e049c5f5
ayushman513 <43521316+ayushman513@users.noreply.github.com>
2021-01-27 16:40:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Newapp from "./Newapp.js"
+import './index.css';
+
+
+ReactDOM.render(
+  <Newapp/>,
+  document.getElementById('root')
+);
